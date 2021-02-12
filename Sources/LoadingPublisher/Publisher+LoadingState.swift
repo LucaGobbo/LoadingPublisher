f0f338f86cccee3b9480aca59c30a2caac567945
Luca Gobbo <luca.gobbo@weareyou.com>
2021-02-12 16:09:59 +0100

    updates function documentation

--- Sources/LoadingPublisher/Publisher+LoadingState.swift
+++ Sources/LoadingPublisher/Publisher+LoadingState.swift
@@ -9,24 +9,28 @@ import Combine
 import Foundation
 
 public extension Publisher where Failure == Never {
+    /// ignore's error & loading state, and only outputs values
     func values<LoadingOutput, LoadingError: Swift.Error>() -> AnyPublisher<LoadingOutput, Never>
         where Output == LoadingState<LoadingOutput, LoadingError>
     {
         compactMap(\.output).eraseToAnyPublisher()
     }
 
+    /// ignore's error & loading state, and outputs a void when value is loaded
     func loaded<LoadingOutput, LoadingError: Swift.Error>() -> AnyPublisher<Void, Never>
         where Output == LoadingState<LoadingOutput, LoadingError>
     {
         compactMap(\.output).map { _ in () }.eraseToAnyPublisher()
     }
 
+    /// ignore's value & loading state, and only outputs failures
     func failures<LoadingOutput, LoadingError: Swift.Error>() -> AnyPublisher<LoadingError, Never>
         where Output == LoadingState<LoadingOutput, LoadingError>
     {
         compactMap(\.failure).eraseToAnyPublisher()
     }
 
+    /// ignores value & error state, and only outputs if the current publisher is loading
     func isLoading<LoadingOutput, LoadingError: Swift.Error>() -> AnyPublisher<Bool, Never>
         where Output == LoadingState<LoadingOutput, LoadingError>
     {
@@ -35,9 +39,7 @@ public extension Publisher where Failure == Never {
 }
 
 public extension Publisher {
-
-    /// Map a Publisher to `AnyLoadingSignal`
-    /// - Returns: the same Publisher but as an `AnyLoadingPublisher`
+    /// Map a Publisher to `AnyLoadingSignal`, and prepends if it's loading
     /// - Note: prepend the `Publisher` with a `loading` state
     func eraseToAnyLoadingPublisher() -> AnyLoadingPublisher<Output, Failure> {
         map { LoadingState<Output, Failure>.loaded($0) }
@@ -47,11 +49,9 @@ public extension Publisher {
     }
 
     /// Map a Publisher to `AnyLoadingSignal`
-    /// - Returns: the same Publisher but as an `AnyLoadingPublisher`
-
     func eraseToAnyLoadingNotPrependLoadingPublisher() -> AnyLoadingPublisher<Output, Failure> {
         map { LoadingState<Output, Failure>.loaded($0) }
-            .catch { Just(LoadingState<Output, Failure>.failure($0)) }    
+            .catch { Just(LoadingState<Output, Failure>.failure($0)) }
             .eraseToAnyPublisher()
     }
 }
