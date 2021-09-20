0f4d79794d4327deb96bec7b65c3419e3b2ba708
Luca Gobbo <luca.gobbo@weareyou.com>
2021-09-20 10:31:13 +0200

    Corrects macOS & WatchOS minimum versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "LoadingPublisher",
-    platforms: [.iOS(.v13), .macOS(.v11), .tvOS(.v13), .watchOS(.v7)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
