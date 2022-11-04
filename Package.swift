16d5b67c39b9fd0923b6cddb05b39769341515a4
Luca Gobbo <luca.gobbo@iodigitial.com>
2022-11-04 18:33:42 +0100

    cleanup

--- Package.swift
+++ Package.swift
@@ -7,23 +7,11 @@ let package = Package(
     name: "LoadingPublisher",
     platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "LoadingPublisher",
-            targets: ["LoadingPublisher"]),
+        .library(name: "LoadingPublisher", targets: ["LoadingPublisher"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LoadingPublisher",
-            dependencies: []),
-        .testTarget(
-            name: "LoadingPublisherTests",
-            dependencies: ["LoadingPublisher"]),
+        .target(name: "LoadingPublisher", dependencies: []),
+        .testTarget(name: "LoadingPublisherTests", dependencies: ["LoadingPublisher"])
     ]
 )
