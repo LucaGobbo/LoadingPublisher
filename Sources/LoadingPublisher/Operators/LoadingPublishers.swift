e853e1664aa6273e0ae06758f26103de7438d7a7
Luca Gobbo <luca.gobbo@iodigitial.com>
2022-11-04 18:37:23 +0100

    more documentation

--- Sources/LoadingPublisher/Operators/LoadingPublishers.swift
+++ Sources/LoadingPublisher/Operators/LoadingPublishers.swift
@@ -7,4 +7,8 @@
 
 import Foundation
 
+/// A namespace for types that serve as loading publishers.
+///
+/// The various operators defined as extensions on LoadingPublisher implement their functionality as classes or
+/// structures that extend this enumeration.
 public enum LoadingPublishers {}
